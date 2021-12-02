22b99a2bf3fa0198fdb4eb65a22e4380e816eb25
Abdullah AlRashoudi <Abdullah.Mansoor.ruh.s@tuwaiq.edu.sa>
2021-12-02 14:19:41 +0300

    start

---
+++ CameraApp/CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Abdullah AlRashoudi on 12/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
