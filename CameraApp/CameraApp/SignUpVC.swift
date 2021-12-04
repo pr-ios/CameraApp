1df0129a88971e50c4c36f20ff916ac8a161033f
Abdullah AlRashoudi <Abdullah.Mansoor.ruh.s@tuwaiq.edu.sa>
2021-12-05 00:02:40 +0300

    update 4

--- CameraApp/CameraApp/SignUpVC.swift
+++ CameraApp/CameraApp/SignUpVC.swift
@@ -20,6 +20,7 @@ class SignUpVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        hideKeyboardWhenTappedAround()
         
         // AppName
        
